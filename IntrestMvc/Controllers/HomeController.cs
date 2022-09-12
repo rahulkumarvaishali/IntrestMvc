19b577313cabec02f70602254f925c9c7631566f
Rahul Kumar <rahulrk00297@gmail.com>
2022-09-12 12:14:06 +0530

    Add project files.

---
+++ IntrestMvc/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using IntrestMvc.Database;
+using IntrestMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntrestMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        harshEntities entobj = new harshEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Intrest()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Intrest( Test obj)
+        {
+           var Result = (obj.principle*obj.rate*obj.samay)/100;
+            ViewBag.Res = Result;
+
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ IntrestMvc/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntrestMvc.Models
+{
+    public class Test
+    {
+        public int id { get; set; }
+        public Nullable<double> principle { get; set; }
+        public Nullable<double> rate { get; set; }
+        public Nullable<double> samay { get; set; }
+    }
+}
